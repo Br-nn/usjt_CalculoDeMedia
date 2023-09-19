deb99a4e334096fe4bdc65e2d2cdd7f92a1ca092
Bruno Dias <brunogd148@gmail.com>
2023-09-18 21:31:26 -0300

    ap05(estrutura_de_repeticao): Media_do_semestre

---
+++ ExemplosBasicos.java
@@ -0,0 +1,26 @@
+public class ExemplosBasicos {
+    public static void main (String [] args){
+        //indent (indentificação)
+        //for, while e do/while
+        //repetição manual de codigo (nao fazer)
+        //System.out.println(1);  
+        //System.out.println(2);
+        //declarar contador
+        //    int cont = 1;
+        //    while(cont <=3) {
+        //        System.out.println(cont);
+        //        cont = cont + 1;
+        //     }     
+         
+        //     for(int cont = 1;cont <=3;cont =cont + 1){
+        //        System.out.println(cont);
+        // }
+        // int cont = 1;
+        // do{
+        //     //%d é um placeholder
+        //     System.out.printf("Valor da vez: %d|n", cont);
+        //     cont += 1;
+        // } while(cont <= 3);
+        
+        }
+    }
